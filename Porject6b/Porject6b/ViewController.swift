607c492407ade90a01c55289bf7690bca14058f5
Andres Quintero <hello@andresq.com>
2019-07-31 20:00:09 -0400

    completed Project 6b with challenges

---
+++ Porject6b/Porject6b/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  Porject6b
+//
+//  Created by Andres Quintero on 7/31/19.
+//  Copyright © 2019 Andres Quintero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var previous: UILabel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = UIColor.cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = UIColor.yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = UIColor.green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = UIColor.orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+       //sets up a randonm color for the background
+        let CGred = CGFloat(Double.random(in: 0.0 ... 1.0))
+        let CGgreen = CGFloat(Double.random(in: 0.0 ... 1.0))
+        let CGblue = CGFloat(Double.random(in: 0.0 ... 1.0))
+        
+        view.backgroundColor = UIColor(red: CGred, green: CGgreen, blue: CGblue, alpha: 0.2)
+        
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//        let metrics = ["labelHeight": 50, "XS_Max_V": 30, "XS_Max_H": 1 ]
+        
+//        for label in viewsDictionary.keys {
+//            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|-(XS_Max_H)-[\(label)]|", options: [], metrics: metrics, views: viewsDictionary))
+//            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|-(XS_Max_V)-[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
+//        }
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+//            label.heightAnchor.constraint(equalToConstant: view.heightAnchor).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.20, constant: -10)
+            
+            if let previous = previous {
+                // we have a previous label – create a height constraint
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                // this is the first label on an iPhone with a notch
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            // set the previous label to be the current one, for the next loop iteration
+            previous = label
+        }
+    }
+
+
+}
+
