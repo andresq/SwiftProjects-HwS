e9e7a1634510dae75fa3994e80437cc54dff5cb0
Andres Quintero <hello@andresq.com>
2019-10-15 08:39:21 -0400

    completed Project10

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Andres Quintero on 10/14/19.
+//  Copyright © 2019 Andres Quintero. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name;
+        self.image = image;
+    }
+
+}
