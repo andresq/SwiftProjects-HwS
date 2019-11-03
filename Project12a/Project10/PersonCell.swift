c3a16595188ea66535c4a8d2f00c833a92326c3b
Andres Quintero <hello@andresq.com>
2019-11-03 18:48:05 -0500

    adds Project 12 (a&b)

---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Andres Quintero on 10/14/19.
+//  Copyright © 2019 Andres Quintero. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
